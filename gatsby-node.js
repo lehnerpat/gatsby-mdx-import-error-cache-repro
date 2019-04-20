f3669eeca1a195a40b4692335b6387b7614c2157
Patrick Lehner <1099818+nevik@users.noreply.github.com>
2019-04-20 10:11:40 +0200

    Create pages for nodes from content/posts/

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-// gatsby-node.js
+const path = require("path");
 const { createFilePath } = require("gatsby-source-filesystem");
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -17,4 +17,48 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value: `/blog${value}`
     });
   }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  // Destructure the createPage function from the actions object
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(
+        `
+          {
+            allMdx {
+              edges {
+                node {
+                  id
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        // this is some boilerlate to handle errors
+        if (result.errors) {
+          console.error(result.errors);
+          reject(result.errors);
+        }
+        // We'll call `createPage` for each result
+        result.data.allMdx.edges.forEach(({ node }) => {
+          createPage({
+            // This is the slug we created before
+            // (or `node.frontmatter.slug`)
+            path: node.fields.slug,
+            // This component will wrap our MDX content
+            component: path.resolve(`./src/components/posts-page-layout.js`),
+            // We can use the values in this context in
+            // our page layout component
+            context: { id: node.id }
+          });
+        });
+      })
+    );
+  });
 };
